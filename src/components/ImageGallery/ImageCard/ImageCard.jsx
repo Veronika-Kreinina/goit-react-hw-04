4756e45256e05bd2e0cccc7c5069028ff9c1d4d1
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-11-17 20:27:40 +0100

    update

--- src/components/ImageGallery/ImageCard/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard/ImageCard.jsx
@@ -2,7 +2,7 @@ import s from "./ImageCard.module.css";
 
 const ImageCard = ({ openModal, alt, regularUrl, smallUrl }) => {
   return (
-    <div className={s.wrapper}>
+    <div className={s.column}>
       <img
         onClick={() => openModal(regularUrl, alt)}
         src={smallUrl}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,11 @@
 import ImageCard from "./ImageCard/ImageCard";
+import s from "./ImageGallery.module.css";
 
 const ImageGallery = ({ images, openModal }) => {
   return (
     <ul>
       {images.map(({ alt, id, smallUrl, regularUrl }) => (
-        <li key={id}>
+        <li key={id} className={s.row}>
           <ImageCard
             openModal={openModal}
             alt={alt}
